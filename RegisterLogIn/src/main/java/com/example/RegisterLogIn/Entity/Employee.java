13c999776638b8f9d9c070a78e66136bd02d612a
Siphephelo Myeni <sipheanele@gmail.com>
2023-11-13 13:47:04 +0200

    Generated constructors, getter and setter methods and toString

--- RegisterLogIn/src/main/java/com/example/RegisterLogIn/Entity/Employee.java
+++ RegisterLogIn/src/main/java/com/example/RegisterLogIn/Entity/Employee.java
@@ -18,4 +18,56 @@ public class Employee {
 
     @Column(name = "password", length = 255)
     private String password;
+
+    public Employee(int employeeid, String employeename, String email, String password) {
+        this.employeeid = employeeid;
+        this.employeename = employeename;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Employee() {
+    }
+
+    public int getEmployeeid() {
+        return employeeid;
+    }
+
+    public void setEmployeeid(int employeeid) {
+        this.employeeid = employeeid;
+    }
+
+    public String getEmployeename() {
+        return employeename;
+    }
+
+    public void setEmployeename(String employeename) {
+        this.employeename = employeename;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeid=" + employeeid +
+                ", employeename='" + employeename + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
